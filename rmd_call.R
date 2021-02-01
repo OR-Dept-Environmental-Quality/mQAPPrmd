f4621cc6fb76eac59483225c8910ca63be70c131
Yuan Grund <yuan.grund@state.or.us>
2021-01-31 22:38:11 -0800

    In progress

--- rmd_call.R
+++ rmd_call.R
@@ -3,7 +3,7 @@ library(rmarkdown)
 setwd("//deqhq1/TMDL/Planning statewide/Temperature_TMDL_Revisions/model_QAPPs/R/data/RData")
 
 data.dir <- "//deqhq1/TMDL/Planning statewide/Temperature_TMDL_Revisions/model_QAPPs/R/data/"
-output.dir <- "//deqhq1/TMDL/Planning statewide/Temperature_TMDL_Revisions/model_QAPPs/R/test_doc/20210128/"
+output.dir <- "//deqhq1/TMDL/Planning statewide/Temperature_TMDL_Revisions/model_QAPPs/R/test_doc/20210131/"
 
 qapp_project_areas <- read.csv(paste0(data.dir,"qapp_project_area.csv"))
 
@@ -28,7 +28,7 @@ for (qapp_project_area in qapp_project_areas$areas) {
   file.name <- qapp_project_areas[which(qapp_project_areas$areas == qapp_project_area),]$file.name
   load(paste0(file.name,".RData"))
   
-  rmarkdown::render(input="E:/PROJECTS/20200810_RyanMichie_TempTMDLReplacement/mQAPPrmd/model_QAPP.Rmd",
+  rmarkdown::render(input="E:/PROJECTS/20200810_RyanMichie_TempTMDLReplacement/R/master/mQAPPrmd/model_QAPP.Rmd",
                     output_format = "word_document",
                     output_dir = output.dir,
                     output_file=paste0("QAPP_",file.name, ".docx"))
